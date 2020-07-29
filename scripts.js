9f2f32c39e8fb2061ce422aeaa029b6dda6ca025
M. A <68448850+mabelprojects@users.noreply.github.com>
2020-07-29 03:26:24 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -37,5 +37,5 @@ $(document).ready(function(){
 //ALERT CONTATO
 function enviou(){
     alert ("Sua mensagem foi enviada. Retornaremos o Contato.\nObrigado!");
-    window.stop();
+    window.location.href = "https://mabelprojects.github.io/BarbaraRamos-MabelAdministradora-2020/";
 }
